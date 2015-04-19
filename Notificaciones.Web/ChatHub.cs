6131ec310b327eac49e3122721156570dcd3c2ef
Oscar Ignacio Osses Arriaza <oscarossesa@gmail.com>
2015-04-19 18:43:47 -0400

    #3
    Se agrega librería SignalR y ejemplo de Chat

---
+++ Notificaciones.Web/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Notificaciones.Web
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //Clients.All.hello();
+            
+            //Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
