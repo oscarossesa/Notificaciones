0caae658c1459c86939b49ba1a0c7e8baeebe752
nicolashermosilla <nhermosilla1987@gmail.com>
2015-04-19 20:47:45 -0300

    fecha

--- Notificaciones.Web/ChatHub.cs
+++ Notificaciones.Web/ChatHub.cs
@@ -8,12 +8,22 @@ namespace Notificaciones.Web
 {
     public class ChatHub : Hub
     {
+        private List<string> messages;
+
+        public ChatHub()
+        {
+            messages = new List<string>();
+        }
+
         public void Send(string name, string message)
         {
+            messages.Add(message);
             //Clients.All.hello();
+
+            
             
             //Call the addNewMessageToPage method to update clients.
-            Clients.All.addNewMessageToPage(name, message);
+            Clients.All.addNewMessageToPage(name, message, DateTime.Now.ToString("hh:mm"));
         }
     }
 }
